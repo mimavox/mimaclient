a06d4cd99a37ee1fa9a64c2621c99d13d7561f21
Andreas Chatzopoulos <andreas.chatzopoulos@gmail.com>
2025-07-04 20:17:58 +0200

    foundation laid

---
+++ screens/main/buttons.gd
@@ -0,0 +1,23 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_btn_2d_button_down() -> void:
+	get_tree().change_scene_to_file("res://screens/mima2D/mima2D.tscn")
+
+
+func _on_btn_3d_button_down() -> void:
+	get_tree().change_scene_to_file("res://screens/mima3D/mima3D.tscn")
+
+
+func _on_btn_test_button_down() -> void:
+	get_tree().change_scene_to_file("res://screens/testdev/testdev.tscn")
