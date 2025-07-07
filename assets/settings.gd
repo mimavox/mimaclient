e4c6e647e6d7532503952458e6dcdbdab51035f2
Andreas Chatzopoulos <andreas.chatzopoulos@gmail.com>
2025-07-07 19:47:09 +0200

    global settings

---
+++ assets/settings.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var baseurl = ""
+var apiurl = ""
+
+func init():
+	baseurl = "http://localhost"
+	apiurl = ""	
--- screens/mima3D/buttons.gd
+++ screens/mima3D/buttons.gd
@@ -3,7 +3,9 @@ extends ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var apiurl = "http://localhost"
+	print(apiurl)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +15,20 @@ func _process(delta: float) -> void:
 
 func _on_btn_back_button_down() -> void:
 	get_tree().change_scene_to_file("res://screens/main/main.tscn")
+
+
+func _on_btn_snapshot_button_down() -> void:
+	print(settings.test)
+	var subviewport = $simulation/SubViewport
+	var texture = subviewport.get_texture()
+
+	if texture:
+		var image = texture.get_image()
+		var path = "res://snapshot.png"  # You can change the name/path
+		var err = image.save_png(path)
+		if err == OK:
+			print("Snapshot saved to: ", path)
+		else:
+			push_error("Failed to save snapshot: %s" % err)
+	else:
+		push_error("SubViewport texture not available!")
